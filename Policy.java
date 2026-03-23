555b7d4a103f8dbc4363537c07ef43ead213ef0c
delophier <delophier@gmail.com>
2026-03-23 16:12:31 -0400

    documentation
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -1,11 +1,20 @@
 public class Policy{
+   /** @param policy_Number policy number of individual */
    int policy_Number;
+   /** @param provider_name insurance company */
    String provider_name;
+   /** @param holder_First first name */
    String holder_First;
+   /** @param holder_Last last name */
    String holder_Last;
+   /** @param holder_Age holders age */
    int holder_Age;
+   /** @param smoker if they are a smoker */
    String smoker;
+   /** @param height holders height */
    double height;
+   double power;
+   /** @param height holders weight */
    double weight;
    public Policy(){
       this.policy_Number=0;
@@ -28,27 +37,35 @@ public class Policy{
       this.weight = weight;
    }
    public int get_Number(){
+   /** @return the policy number */
       return policy_Number;
    }
    public String get_Company(){
+   /** @return the policy company */
       return provider_name;
    }
    public String get_Fisrt(){
+   /** @return the policy holders first name */
       return holder_First;
    }
    public String get_Last(){
+   /** @return the policy holders last name */
       return holder_Last;
    }
    public int get_Age(){
+   /** @return the policy holders age */
       return holder_Age;
    }
    public String get_Smoker(){
+   /** @return if the holder is a smoker */
       return smoker;
    }
    public double get_Height(){
+   /** @return the height */
       return height;
    }
    public double get_Weight(){
+   /** @return the weight */
       return weight;
    }
    public void set_Number(int number){
@@ -91,6 +108,7 @@ public class Policy{
       if (BMI > 35){
          cost +=(BMI-35)*20;
       }
+      /** @return the cost of the policy */
       return cost;
    } 
 
