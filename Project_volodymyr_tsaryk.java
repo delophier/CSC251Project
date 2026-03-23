1d2dd332fe1d87286cdae769e72846f7e2636c04
delophier <delophier@gmail.com>
2026-03-23 16:51:05 -0400

    Update demo class
    Modified to read input from a file

--- Project_volodymyr_tsaryk.java
+++ Project_volodymyr_tsaryk.java
@@ -1,46 +1,35 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Project_volodymyr_tsaryk
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
-      Scanner input = new Scanner(System.in);
-      int number;
+      File file = new File("PolicyInformation.txt");
+      ArrayList<Policy> policies = new ArrayList<>();
+      Scanner inputFile = new Scanner(file);
+
+      int policy;
       String name;
       String first;
       String last;
       int age;
       String smoker;
       double height;
-      double weight;
-      System.out.println("Please enter the Policy Number: ");
-      number = input.nextInt();
-      System.out.println();
-      input.nextLine();
-      System.out.println("Please enter the Provider Name: ");
-      name = input.nextLine();
-      System.out.println();
-      System.out.println("Please enter the Policyholder’s First Name: ");
-      first = input.nextLine();
-      System.out.println();
-      System.out.println("Please enter the Policyholder’s Last Name: ");
-      last = input.nextLine();
-      System.out.println();
-      System.out.println("Please enter the Policyholder’s Age: ");
-      age = input.nextInt();
-      input.nextLine();
-      System.out.println();
-      System.out.println("Please enter the Policyholder’s Smoking Status (smoker/non-smoker): ");
-      smoker = input.nextLine();
-      System.out.println();
-      System.out.println("Please enter the Policyholder’s Height (in inches): ");
-      height = input.nextDouble();
-      System.out.println();
-      System.out.println("Please enter the Policyholder’s Weight (in pounds): ");
-      weight = input.nextDouble();
-      System.out.println();
-      Policy test = new Policy(number,name,first,last,age,smoker,height,weight);
-      System.out.println("Policy Number: " + test.get_Number());
+      double weight;      
+      while(inputFile.hasNext()){
+         policy = inputFile.nextInt();
+         name = inputFile.nextLine();
+         first = inputFile.nextLine();
+         last = inputFile.nextLine();
+         age = inputFile.nextInt();
+         smoker = inputFile.nextLine();
+         height = inputFile.nextInt();
+         weight = inputFile.nextInt();
+         inputFile.nextLine();
+         policies.add(new Policy(policy,name,first,last,age,smoker,height,weight));
+         
+      }
+      /**System.out.println("Policy Number: " + test.get_Number());
       System.out.println();
       System.out.println("Provider Name: " + test.get_Company());
       System.out.println();
@@ -58,7 +47,8 @@ public class Project_volodymyr_tsaryk
       System.out.println();
       System.out.printf("Policyholder’s BMI: %.2f%n", test.BMICalc());
       System.out.println();
-      System.out.printf("Policy Price: $%.2f%n",test.policyCost());
+      System.out.printf("Policy Price: $%.2f%n",test.policyCost()); */
+      inputFile.close();
    }
 
 }
