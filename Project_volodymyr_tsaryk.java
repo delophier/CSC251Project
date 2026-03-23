42091d635647352f6a0da4a745d216c118f67ee4
delophier <delophier@gmail.com>
2026-03-23 16:52:48 -0400

    demo change
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_volodymyr_tsaryk.java
+++ Project_volodymyr_tsaryk.java
@@ -29,25 +29,37 @@ public class Project_volodymyr_tsaryk
          policies.add(new Policy(policy,name,first,last,age,smoker,height,weight));
          
       }
-      /**System.out.println("Policy Number: " + test.get_Number());
-      System.out.println();
-      System.out.println("Provider Name: " + test.get_Company());
-      System.out.println();
-      System.out.println("Policyholder’s First Name: " + test.get_Fisrt());
-      System.out.println();
-      System.out.println("Policyholder’s Last Name: " + test.get_Last());
-      System.out.println();
-      System.out.println("Policyholder’s Age: " + test.get_Age());
-      System.out.println();
-      System.out.println("Policyholder’s Smoking Status: " + test.get_Smoker());
-      System.out.println();
-      System.out.println("Policyholder’s Height: " + test.get_Height());
-      System.out.println();
-      System.out.println("Policyholder’s Weight: " + test.get_Weight());
-      System.out.println();
-      System.out.printf("Policyholder’s BMI: %.2f%n", test.BMICalc());
-      System.out.println();
-      System.out.printf("Policy Price: $%.2f%n",test.policyCost()); */
+      int smokerCount=0;
+      int nonSmoker= 0;
+      for (Policy test: policies){
+         System.out.println("Policy Number: " + test.get_Number());
+         System.out.println();
+         System.out.println("Provider Name: " + test.get_Company());
+         System.out.println();
+         System.out.println("Policyholder’s First Name: " + test.get_Fisrt());
+         System.out.println();
+         System.out.println("Policyholder’s Last Name: " + test.get_Last());
+         System.out.println();
+         System.out.println("Policyholder’s Age: " + test.get_Age());
+         System.out.println();
+         System.out.println("Policyholder’s Smoking Status: " + test.get_Smoker());
+         System.out.println();
+         if(test.get_Smoker().equals("smoker")){
+            ++smokerCount;
+         }
+         else{
+            ++nonSmoker;
+         }
+         System.out.println("Policyholder’s Height: " + test.get_Height());
+         System.out.println();
+         System.out.println("Policyholder’s Weight: " + test.get_Weight());
+         System.out.println();
+         System.out.printf("Policyholder’s BMI: %.2f%n", test.BMICalc());
+         System.out.println();
+         System.out.printf("Policy Price: $%.2f%n",test.policyCost());
+      }
+      System.out.println("The number of policies with a smoker is: " + smokerCount);
+      System.out.println("The number of policies with a non-smoker is: " + nonSmoker);
       inputFile.close();
    }
 
